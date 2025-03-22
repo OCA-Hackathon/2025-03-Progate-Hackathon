3b474e7cf6eb57ef4a3439243af2bb31d3ad418c
JHSAND <shotaro0106t@gmail.com>
2025-03-23 08:06:29 +0900

    fix: /signup done

--- src/app/register/components/RegisterForm.tsx
+++ src/app/register/components/RegisterForm.tsx
@@ -1,16 +1,43 @@
 "use client";
 import Button from "@/app/components/ui/Button";
 import { useState } from "react";
+import { signUp } from "aws-amplify/auth";
+import { useRouter } from "next/navigation";
 
 export default function RegisterForm() {
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState<"success" | "error" | "">("");
+  const router = useRouter();
 
   const handleRegister = async () => {
     console.log("Registering...");
     console.log("Username:", username);
     console.log("Password:", password);
+
+    try {
+      const { isSignUpComplete } = await signUp({
+        username,
+        password,
+        options: {
+          userAttributes: {
+            email: ""
+          },
+        },
+      });
+
+      console.log("isSignUpComplete:", isSignUpComplete);
+      if (isSignUpComplete) {
+        setMessage("Registration successful. Please login.");
+        setMessageType("success");
+      }
+    } catch (error) {
+      console.error("Register error:", error);
+      setMessage("Registration failed.");
+      setMessageType("error");
+    }
   }
 
 
@@ -18,6 +45,19 @@ export default function RegisterForm() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#1C1C1C] text-white">
       <div className="w-full max-w-md p-8 rounded-xl bg-[#252525] shadow-2xl border border-[#333333]">
         <h1 className="text-3xl font-bold mb-8 text-center bg-gradient-to-r from-purple-400 to-blue-500 bg-clip-text text-transparent">REGISTER</h1>
+
+        {message && (
+          <div className={`text-center mb-4 ${
+            messageType === "success"
+              ? "text-green-600"
+              : messageType === "error"
+              ? "text-red-600"
+              : ""
+          }`}>
+            {message}
+          </div>
+        )}
+
 
         <div className="space-y-6">
           <div className="relative">
